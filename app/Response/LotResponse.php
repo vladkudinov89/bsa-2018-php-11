13fcb405ae697d83f11a503a13fe6e84286f3655
vladkudinov89 <vlad.kudinov.89@gmail.com>
2018-07-20 12:45:53 +0300

    Work#0. Realize Response

---
+++ app/Response/LotResponse.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vlad
+ * Date: 20.07.18
+ * Time: 12:45
+ */
+
+namespace App\Response;
+
+
+class LotResponse implements Contracts\LotResponse
+{
+    public function getId(): int
+    {
+        // TODO: Implement getId() method.
+    }
+
+    public function getUserName(): string
+    {
+        // TODO: Implement getUserName() method.
+    }
+
+    public function getCurrencyName(): string
+    {
+        // TODO: Implement getCurrencyName() method.
+    }
+
+    public function getAmount(): float
+    {
+        // TODO: Implement getAmount() method.
+    }
+
+    public function getDateTimeOpen(): string
+    {
+        // TODO: Implement getDateTimeOpen() method.
+    }
+
+    public function getDateTimeClose(): string
+    {
+        // TODO: Implement getDateTimeClose() method.
+    }
+
+    public function getPrice(): string
+    {
+        // TODO: Implement getPrice() method.
+    }
+
+}
