42144c83ebf37fe5c934c28c7370eecdceab120f
vladkudinov89 <vlad.kudinov.89@gmail.com>
2018-07-20 13:47:01 +0300

    Work#0. Add logic WalletService

--- app/Service/WalletService.php
+++ app/Service/WalletService.php
@@ -1,34 +1,65 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: vlad
- * Date: 20.07.18
- * Time: 12:40
- */
 
 namespace App\Service;
 
 
 use App\Entity\Money;
 use App\Entity\Wallet;
+use App\Repository\Contracts\MoneyRepository;
+use App\Repository\Contracts\WalletRepository;
 use App\Request\Contracts\CreateWalletRequest;
 use App\Request\Contracts\MoneyRequest;
 
 class WalletService implements Contracts\WalletService
 {
+    private $moneyRepository;
+    private $walletRepository;
+
+    public function __construct(MoneyRepository $moneyRepository , WalletRepository $walletRepository)
+    {
+        $this->moneyRepository = $moneyRepository;
+        $this->walletRepository = $walletRepository;
+    }
+
     public function addWallet(CreateWalletRequest $walletRequest): Wallet
     {
-        // TODO: Implement addWallet() method.
+        $wallet = new Wallet();
+        $wallet->user_id = $walletRequest->getUserId();
+        return $this->walletRepository->add($wallet);
+
     }
 
     public function addMoney(MoneyRequest $moneyRequest): Money
     {
-        // TODO: Implement addMoney() method.
+        $walletId = $moneyRequest->getWalletId();
+        $amount = $moneyRequest->getAmount();
+        $currencyId = $moneyRequest->getCurrencyId();
+
+        $money = $this->moneyRepository->findByWalletAndCurrency($walletId , $currencyId);
+
+        if($money === null){
+            $money = new Money;
+            $money->wallet_id = $walletId;
+            $money->currency_id = $currencyId;
+            $money->amount = $amount;
+        } else {
+            $money->amount += $amount;
+        }
+
+        return $this->moneyRepository->save($money);
     }
 
     public function takeMoney(MoneyRequest $moneyRequest): Money
     {
-        // TODO: Implement takeMoney() method.
+        $walletId = $moneyRequest->getWalletId();
+        $amount = $moneyRequest->getAmount();
+        $currencyId = $moneyRequest->getCurrencyId();
+
+        $money = $this->moneyRepository->findByWalletAndCurrency($walletId , $currencyId);
+
+        $money->amount -= $amount;
+
+        return $this->moneyRepository->save($money);
     }
 
 }
