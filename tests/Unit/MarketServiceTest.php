916ea26d0c619d7876db2986eb38ef8378ceaec1
vladkudinov89 <vlad.kudinov.89@gmail.com>
2018-07-27 19:18:33 +0300

    Work#1.Add testGetLot() & testGetLotList()

--- tests/Unit/MarketServiceTest.php
+++ tests/Unit/MarketServiceTest.php
@@ -21,6 +21,7 @@ use App\Repository\WalletRepository;
 use App\Request\AddCurrencyRequest;
 use App\Request\AddLotRequest;
 use App\Request\BuyLotRequest;
+use App\Response\Contracts\LotResponse;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
@@ -50,6 +51,24 @@ class MarketServiceTest extends TestCase
         $this->walletRepository = $this->createMock(WalletRepository::class);
         $this->moneyRepository = $this->createMock(MoneyRepository::class);
         $this->tradeRepository = $this->createMock(TradeRepository::class);
+
+//        $this->currencyRepository->method('add')->will($this->returnCallback(function ($arg) {
+//            return self::returnModelWithId($arg);
+//        }));
+//
+//        $this->lotRepository->method('add')->will($this->returnCallback(function ($arg) {
+//            return self::returnModelWithId($arg);
+//        }));
+//
+//        $this->walletRepository->method('add')->will($this->returnCallback(function ($arg) {
+//            return self::returnModelWithId($arg);
+//        }));
+//        $this->moneyRepository->method('save')->will($this->returnCallback(function ($arg) {
+//            return self::returnModelWithId($arg);
+//        }));
+//        $this->tradeRepository->method('add')->will($this->returnCallback(function ($arg) {
+//            return self::returnModelWithId($arg);
+//        }));
 
 
         $this->lotRepository->method('add')->will($this->returnArgument(0));
@@ -111,17 +130,18 @@ class MarketServiceTest extends TestCase
 
 
         $currency = factory(Currency::class)->make(['id' => 1]);
-        $user = factory(User::class)->make(['id' => 1]);
+
         $seller = factory(User::class)->make(['id' => 2]);
 
         $lot = factory(Lot::class)->make([
-            'id' => 1,
+            'id' => 5,
             'currency_id' => $currency->id,
             'seller_id' => $seller->id,
             'date_time_open' => Carbon::now(),
             'date_time_close' => Carbon::tomorrow(),
-            'price' => 4
+            'price' => 25
         ]);
+
         $buyer = factory(User::class)->make(['id' => 1]);
 
         $buyerWallet = factory(Wallet::class)->make(['id' => 1,'user' => $buyer->id]);
@@ -142,18 +162,95 @@ class MarketServiceTest extends TestCase
             'id' => 2,
             'wallet_id' => $sellerWallet->id,
             'currency_id' => $currency->id,
-            'amount' => 500
+            'amount' => 1000
             ]);
 
-
-        $buyLotRequest = new BuyLotRequest($user->id, $lot->id, $lot->price);
-
+        $buyLotRequest = new BuyLotRequest($buyer->id, $lot->id, $lot->price);
+//        dd($buyLotRequest);
         $trade = $this->marketService->buyLot($buyLotRequest);
-        $this->assertEquals($user->id, $trade->user_id);
-        $this->assertEquals($lot->id, $trade->lot_id);
-        $this->assertEquals($lot->price, $trade->amount);
+//        dd($trade);
+        $this->assertEquals($buyLotRequest->getUserId(), $trade->user_id);
+        $this->assertEquals($buyLotRequest->getLotId(), $trade->lot_id);
+        $this->assertEquals($buyLotRequest->getAmount(), $trade->amount);
         $this->assertInstanceOf(Trade::class, $trade);
         Mail::assertSent(TradeCreated::class);
     }
+
+    public function testGetLot()
+    {
+        $currency = factory(Currency::class)->make(['id' => 1]);
+        $user = factory(User::class)->make(['id' => 1]);
+        $wallet = factory(Wallet::class)->make([
+            'id' => 1,
+            'user_id' => $user->id
+        ]);
+        $money = factory(Money::class)->make([
+            'id' => 1,
+            'wallet_id' => $wallet->id,
+            'currency_id' => $currency->id,
+            'amount' => 1000
+        ]);
+        $lot = factory(Lot::class)->make([
+            'id' => 1,
+            'currency_id' => $currency->id,
+            'seller_id' => $user->id,
+            'date_time_open' => Carbon::now(),
+            'date_time_close' => Carbon::tomorrow(),
+            'price' => 150
+        ]);
+
+        $this->lotRepository->method('getById')->willReturn($lot);
+        $this->currencyRepository->method('getById')->willReturn($currency);
+        $this->userRepository->method('getById')->willReturn($user);
+        $this->walletRepository->method('findByUser')->willReturn($wallet);
+        $this->moneyRepository->method('findByWalletAndCurrency')->willReturn($money);
+
+        $lotResponse = $this->marketService->getLot($lot->id);
+
+        $this->assertEquals($lot->id, $lotResponse->getId());
+        $this->assertEquals($user->name, $lotResponse->getUserName());
+        $this->assertEquals($currency->name, $lotResponse->getCurrencyName());
+        $this->assertEquals($lot->date_time_open->format('Y/m/d H:i:s'), $lotResponse->getDateTimeOpen());
+        $this->assertEquals($lot->date_time_close->format('Y/m/d H:i:s'), $lotResponse->getDateTimeClose());
+        $this->assertEquals($lot->price, $lotResponse->getPrice());
+    }
+
+    public function testGetLotList()
+    {
+        $currency = factory(Currency::class)->make(['id' => 1]);
+        $user = factory(User::class)->make(['id' => 2]);
+        $wallet = factory(Wallet::class)->make(['id' => 1,'user' => $user->id]);
+
+        $money = factory(Money::class)->make([
+            'id' => 1,
+            'wallet_id' => $wallet->id,
+            'currency_id' => $currency->id,
+            'amount' => 1000
+        ]);
+        $lot = factory(Lot::class)->make([
+            'id' => 5,
+            'currency_id' => $currency->id,
+            'seller_id' => $user->id,
+            'date_time_open' => Carbon::now(),
+            'date_time_close' => Carbon::tomorrow(),
+            'price' => 100
+        ]);
+        $this->lotRepository->method('getById')->willReturn($lot);
+        $this->currencyRepository->method('getById')->willReturn($currency);
+        $this->userRepository->method('getById')->willReturn($user);
+        $this->walletRepository->method('findByUser')->willReturn($wallet);
+        $this->moneyRepository->method('findByWalletAndCurrency')->willReturn($money);
+        $this->lotRepository->method('findAll')->willReturn([$lot, $lot]);
+        $lotList = $this->marketService->getLotList();
+        foreach ($lotList as $lot) {
+            $this->assertInstanceOf(LotResponse::class, $lot);
+        }
+    }
+
+//    private static function returnModelWithId(Model $model)
+//    {
+//        $model->id = random_int(1, 100);
+//        return $model;
+//    }
 
 }
